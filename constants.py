145120bdec0f2d867ca449b709dfd6ed531cbc05
Finkch <skyler@thealdersons.org>
2023-12-19 15:35:06 -0800

    Added additional helpful constatns

--- constants.py
+++ constants.py
@@ -1,6 +1,9 @@
 # A bunch of constant values
 
-G = 6.67e-11
+G =             6.67e-11
 
-au = 1.496e11
-earth_speed = 29951.68
+au =            1.496e11
+earth_speed =   29951.68
+
+sun_mass =      3.995e30
+sun_radius =    696340e3
