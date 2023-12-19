c4ad57dfd50ebb7310bae4da265f327bbdafe273
Finkch <skyler@thealdersons.org>
2023-12-19 12:18:14 -0800

    Added actor superclass to spacecraft
    Also functions to help automatically update the two

--- spacecraft.py
+++ spacecraft.py
@@ -7,11 +7,45 @@
 #   Reactor
 # All these components have some mass
 
+import vector as v
+
+
+class actor(object):
+    def __init__(self, name, mass):
+
+        self.spacetime = v.spacetime()
+
+        self.mass = mass
+
+        # The most crucial part: the name
+        self.name = name
+
+    # Updates the craft
+    def __call__(self, time_step):
+        self.spacetime(time_step)
+
+    def __str__(self):
+        return self.name + "\n" + str(self.spacetime)
+    
+
+
+    # Exerts a force on the actor
+    def force(self, force):
+        # Imparts the force as acceleration on the craft
+        self.spacetime.acceleration += force / self.mass
 
 
 # This is the core, the glue that holds everything together
-class spacecraft:
-    def __init__(self, core_mass, thruster, ionizer, scoop, tank, reactor):
+class spacecraft(actor):
+    def __init__(self, name, core_mass, thruster, ionizer, scoop, tank, reactor):
+
+        # Requires an angular orientation
+        #   Wrap this in a class?
+        #   Yeah, then can have functions to convert magnitude to directions
+        self.phi = 0
+        self.theta = 0
+
+
 
         # Creates the craft from the components
         self.core_mass = core_mass
@@ -22,21 +56,24 @@ class spacecraft:
         self.reactor = reactor
 
         # Gets the mass of the craft
-        self.mass = 0
-        self.mass = self.get_mass()
+        super().__init__(name, self.get_mass())
+
     
     # Returns the current mass of the craft
     def get_mass(self):
         mass = 0
         mass += self.core_mass
-        mass += self.thruster.mass
-        mass += self.ionizer.mass
-        mass += self.scoop.mass
-        mass += self.tank.mass
-        mass += self.reactor.mass
+        mass += self.thruster.get_mass()
+        mass += self.ionizer.get_mass()
+        mass += self.scoop.get_mass()
+        mass += self.tank.get_mass()
+        mass += self.reactor.get_mass()
 
         return mass
-    
+
+
+
+
 
 # What produces the thrust
 #   Accepts ionized gas and power
@@ -49,6 +86,9 @@ class thruster:
         self.v_e = v_e    # Exhaust velocity
         self.thrust_per = thrust_per
         self.power_per = power_per
+    
+    def get_mass(self):
+        return self.mass
 
 # Ionizes inputted gas
 #   Accepts deionized gas and power
@@ -61,6 +101,9 @@ class ionizer:
         self.power_per = power_per
         self.in_flow = in_flow
         self.out_flow = out_flow
+    
+    def get_mass(self):
+        return self.mass
 
 # Scoops up gas from the interstellar medium
 #   Accepts power
@@ -72,17 +115,23 @@ class scoop:
 
         self.power = power
         self.radius = radius
+    
+    def get_mass(self):
+        return self.mass
 
 # Hold fuel
 #   (Optionally) accepts gas
 #   Outputs gas
 class tank:
-    def __init__(self, mass, fuel, max_fuel):
+    def __init__(self, mass, max_fuel_mass, fuel):
         
         self.mass = mass
 
+        self.max_fuel = max_fuel_mass
         self.fuel = fuel
-        self.max_fuel = max_fuel
+
+    def get_mass(self):
+        return self.mass + self.fuel
 
 # Creates power
 #   (Depends on type) accepts sun or fuel
@@ -93,3 +142,6 @@ class reactor:
         self.mass = mass
 
         self.generation = generation
+    
+    def get_mass(self):
+        return self.mass
