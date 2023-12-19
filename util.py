67d3ac5d8866f555686ab030ffa9f483328e50b5
Finkch <skyler@thealdersons.org>
2023-12-19 12:16:25 -0800

    Added functionality to util
    Notably added a function to convert from radial to cartesian

--- util.py
+++ util.py
@@ -1,11 +1,39 @@
 # Several helpful utility functions
-import math
+import numpy as np
+import vector as v
 
 # Returns the hypotenuse of a vector
 def hypo(vec):
-    
+
     # Sums the squares of each component and returns their square root
     hypo = 0
-    for component in vec:
+    for component in vec():
         hypo += component ** 2
-    return math.sqrt(hypo)
+    return np.sqrt(hypo)
+
+
+# Returns the theta component of the radial vector
+def theta(vec):
+
+    # theta = arccos(z / r)
+    return np.arccos(vec.z / hypo(vec))
+
+# Returns the phi component of the radial vector
+def phi(vec):
+
+    # phi = sgn(y) * arccos(x / rho)
+    return np.sign(vec.y) * np.arccos(vec.x / hypo(v.vector(vec.x, vec.y)))
+
+
+# Splits a radial vector into its cartesian components.
+# It is assumed that vec is a vector with the same orientation
+# but a different magnitude
+def radial_to_cartesian(radial, vec):
+    th = theta(vec)
+    ph = phi(vec)
+
+    return v.vector(
+        radial * np.sin(th) * np.cos(ph),
+        radial * np.sin(th) * np.sin(ph),
+        radial * np.cos(th)
+    )
