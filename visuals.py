d5875ff9b8cfcc08edfac0f19593969167ea0ed0
Finkch <skyler@thealdersons.org>
2023-12-19 17:37:55 -0800

    Added draw function to visuals

--- visuals.py
+++ visuals.py
@@ -3,6 +3,7 @@ import constants as c
 from util import *
 
 MIN_SIZE = c.au
+MIN_RADIUS = 2
 PADDING = 1.2
 
 
@@ -16,4 +17,44 @@ class shape:
 def init_visuals(width, height):
     pygame.init()
     screen = pygame.display.set_mode((width, height))
-    return screen    return screen
+    return screen
+
+# Draws everything
+#   focus is the actor at the centre of the display
+def draw(screen, focus, actors):
+    
+    # Clears screen to black
+    screen.fill("black")
+
+    # Calculates the scale using the greates seperation distance to the focus
+    max_distance = max([abs(dif(focus.pos(), actor.pos())) for actor in actors])
+    if max_distance < MIN_SIZE:
+        max_distance = MIN_SIZE
+
+    # Finds the maximum distance between the crafts and the PoR.
+    # This distance is used to scale everything to fit on screen.
+    # Extra factor of two is for half the screen
+    scale = screen.get_width() / (max_distance * PADDING) / 2
+
+
+    # Draws each actor
+    for actor in actors:
+
+        # Scales the radius a bit; enforces minimum radius
+        radius = actor.shape.radius * np.log10(actor.shape.radius) * scale
+        if radius < MIN_RADIUS:
+            radius = MIN_RADIUS
+
+
+        # Draws the shape
+        pixel_position = (((actor.pos() - focus.pos()) * scale) + v.vector(screen.get_width() / 2, screen.get_height() / 2, 0)).plane()
+        pygame.draw.circle(screen, "white", pixel_position, radius)    
+
+    # Draws?
+    pygame.display.flip()
+    
+
+
+
+
+
