7de0dc1fe4b3554bf7d1672614906752bae58159
Finkch <skyler@thealdersons.org>
2023-12-21 14:08:23 -0800

    Created a keyboard class

---
+++ keyboard.py
@@ -0,0 +1,10 @@
+# This class handles keyboard inputs
+
+class keyboard:
+    def __init__(self):
+        pass
+
+
+    # Handles accepting input and performing an action
+    def __call__(self):
+        pass
