ab7cca397eabc93e8c035171724a46b88b0748f7
Finkch <skyler@thealdersons.org>
2023-12-22 12:25:02 -0800

    Zoom has been improved; may now shift focus

--- zoom.py
+++ zoom.py
@@ -3,23 +3,49 @@
 import vector as v
 
 class Zoom:
-    def __init__(self, initial_zoom, timer, focus = v.Vector()):
+    def __init__(self, initial_zoom, timer, actors, position = v.Vector()):
         
         # Sets initial zoom
         self.goal = initial_zoom
 
         # For easy references
         self.timer = timer
-        
-        # Sets the initial focus
-        self.update_focus(focus)
+        self.actors = actors
 
+        # Sets initial focus
+        self.focus = 0
+        self.focus_at(position)
 
-    # Updates the focus of the zoom
-    def update_focus(self, focus):
-        if isinstance(focus, v.Vector):
-            self.focus = None
-            self.pos = focus
-        else:
-            self.focus = focus
-            self.pos = focus.pos()
+    # Focuses on the next actor
+    def next(self):
+
+        # Goes to the next
+        self.focus += 1
+
+        # Handles the boundaries
+        if self.focus >= len(self.actors):
+            self.focus = 0
+
+        # Updates
+        self.update_focus()
+    
+    # Focuses on the previous actor
+    def previous(self):
+
+        # Goes to the previous
+        self.focus -= 1
+
+        # Handles the boundaries
+        if self.focus < 0:
+            self.focus = len(self.actors) - 1
+
+        # Updates
+        self.update_focus()
+    
+    # Sets position to be that of the actor
+    def update_focus(self):
+        self.focus_at(self.actors[self.focus].pos())
+
+    # Focuses at a particular vector
+    def focus_at(self, position = v.Vector()):
+        self.position = position
