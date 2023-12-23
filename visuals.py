a53931604aa2caf25c3e922ca71b9212ab57251e
Finkch <skyler@thealdersons.org>
2023-12-22 16:21:11 -0800

    Bug fix
    Called wrong variable (theta instead of phi) when drawing orientation indicator

--- visuals.py
+++ visuals.py
@@ -288,7 +288,7 @@ def draw_actors(screen, zoom, actors, scale):
             # Gets the position of the orientation indicator
             distance = 2
             indicator = (
-                    pixel_position[0] + (radius * 3 / 2 + distance) * np.cos(actor.apos().theta), 
+                    pixel_position[0] + (radius * 3 / 2 + distance) * np.cos(actor.apos().phi), 
                     pixel_position[1] + (radius * 3 / 2 + distance) * np.sin(actor.apos().phi)
                 )
 
