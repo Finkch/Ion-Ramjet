e7b157bddb8b3671910ec61a8eabe346dd3ed427
Finkch <skyler@thealdersons.org>
2023-12-23 23:55:44 -0800

    Added functions to be able to return ship parts
    These are currently empty

--- storage.py
+++ storage.py
@@ -1,1 +1,39 @@
 # This is a place to store data, such as spacecrafts or planets
+
+
+# GENERATORS
+def thrusters(name):
+    match name:
+        case 'MPDT-thruster':
+            return None
+        
+def ionizers(name):
+    match name:
+        case 'MPDT-ionizer':
+            return None
+
+def reactors(name):
+    match name:
+        case 'MMRTG': # Used on Perseverence and Curiosity!
+            return None
+
+def scoops(name):
+    match name:
+        case 'Bussard\'s Scoop':
+            return None
+        
+
+        
+# REGULATORS
+def tanks(name):
+    match name:
+        case 'MPDT Hydrogen Tank':
+            return None
+        case 'MPDT Proton Tank':
+            return None
+
+def batteries(name):
+    match name:
+        case 'Z100':
+            return None
+        
