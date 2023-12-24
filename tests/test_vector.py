41d6bc7b841a37aa61c6787f261ca49b5f786bdd
Finkch <skyler@thealdersons.org>
2023-12-23 19:28:12 -0800

    Added tests for vectors
    Doesn't yet cover dot or cross or the angles

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -2,6 +2,7 @@
 
 import unittest
 from vector import *
+import numpy as np
 
 
 class TestStringMethods(unittest.TestCase):
@@ -10,3 +11,57 @@ class TestStringMethods(unittest.TestCase):
     def test_instantiate_and_call(self):
         vec = Vector(5, 4, 3)
         self.assertEqual(vec(), [5, 4, 3])
+
+    def two_vectors(self):
+        return Vector(5, 4, 3), Vector(-1, -2, -3)
+
+    def test_add(self):
+        vec1, vec2 = self.two_vectors()
+        result = vec1 + vec2
+
+        self.assertEqual(result(), [4, 2, 0])
+
+    def test_sub(self):
+        vec1, vec2 = self.two_vectors()
+        result = vec1 - vec2
+
+        self.assertEqual(result(), [6, 6, 6])
+
+    def test_mult(self):
+        vec1 = self.two_vectors()
+        mult = -3
+        result1 = vec1 * mult
+        result2 = mult * vec1
+
+        self.assertEqual(result1(), result2())
+        self.assertEqual(result1(), [-15, -12, -9])
+        self.assertEqual(result2(), [-15, -12, -9])
+
+    def test_div(self):
+        vec1 = self.two_vectors()
+        div1 = 60
+        div2 = 10
+        result1 = div1 / vec1
+        result2 = vec1 / div2
+
+        self.assertEqual(result1(), [12, 15, 20])
+        self.assertEqual(result2(), [0.5, 0.4, 0.3])
+    
+    def test_pow(self):
+        vec1 = self.two_vectors()
+        power = 3
+        result = vec1 ** power
+
+        self.assertEqual(result(), [125, 64, 27])
+
+    def test_neg(self):
+        vec1 = self.two_vectors()
+        result = -vec1
+
+        self.assertEqual(result(), [-5, -4, -3])
+    
+    def test_hypo(self):
+        vec1 = self.two_vectors()
+        result = vec1.hypo()
+
+        self.assertAlmostEqual(result, np.sqrt(5 ** 2 + 4 ** 2 + 3 ** 2))
