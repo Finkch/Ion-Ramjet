fa203e971a02985db0cd3915e4968cdf53aec6fa
Finkch <skyler@thealdersons.org>
2023-12-24 23:34:02 -0800

    Main now uses storage for universe setup

--- main.py
+++ main.py
@@ -38,12 +38,9 @@ def setup():
 
 
     # Grabs the actors
-    #craft = st.spacecrafts('S-IVB')
-    craft = st.spacecrafts('ioRam-0')
-    #actors = [st.stars('Sol'), craft]
-    actors = [craft]
-    
-    craft.spacetime.velocity = v.Vector()
+    actors, craft = st.universes('Basic')
+
+
 
     zoom = o.Zoom(0, timer, actors)
     keybboard = kb.Keyboard(timer, zoom, actors, craft)
