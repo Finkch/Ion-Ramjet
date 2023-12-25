749b38e2a6be4e34e8c9a627369a7acb870af188
Finkch <skyler@thealdersons.org>
2023-12-24 16:00:18 -0800

    Added H densities in vacuum to constants

--- constants.py
+++ constants.py
@@ -16,3 +16,9 @@ sun_mass =      3.995e30
 sun_radius =    696340e3
 
 H =             1.6735575e-27
+
+# Number of H per m^3 (100 per cm^3)
+vacuum_H_density = 1000000
+
+# Mass of hydrogen per m^3
+vacuum_H_mass_density = H * vacuum_H_density
