1b98fdd9d0fdb13c224da347ef0ad1c4e28318cd
Finkch <skyler@thealdersons.org>
2023-12-27 19:43:24 -0800

    I'm an doofus, I'm on the wrong repo. That's one issue of having three windows open for three repos... (removed previous push)

--- constants.py
+++
@@ -1,9 +0,0 @@
-# Some constants, before they're pushed to the shared lib
-
-import numpy as np
-
-e_0 = 8.8541878128e-12 # F / m
-mu_0 = 1.25663706212e-12 # N / A^2
-
-k_e = 1 / (4 * np.pi * e_0) # m / F
-k_m = mu_0 / (4 * np.pi)    # N / A^2
