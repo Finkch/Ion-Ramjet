d6620ce7b7ce87f3da0e9720f03c7a6fcdb75168
Finkch <skyler@thealdersons.org>
2023-12-30 13:27:47 -0800

    Moved keys from finchlib.Keyboard into IonRamjetKey

--- keyboard.py
+++ keyboard.py
@@ -10,6 +10,17 @@ class IonRamjetKeyboard(Keyboard):
 
         # Adds the rest of the keys
         self.keys = self.keys | {            
+            'up':           {'count': 0, 'key': pygame.K_UP,            'type': self.pressed,       'function': self.clock.fasterer}, 
+            'down':         {'count': 0, 'key': pygame.K_DOWN,          'type': self.pressed,       'function': self.clock.slowerer}, 
+            'right':        {'count': 0, 'key': pygame.K_RIGHT,         'type': self.pressed,       'function': self.clock.faster}, 
+            'left':         {'count': 0, 'key': pygame.K_LEFT,          'type': self.pressed,       'function': self.clock.slower}, 
+            'return':       {'count': 0, 'key': pygame.K_RETURN,        'type': self.held,          'function': self.zoom.decrease}, 
+            'rshift':       {'count': 0, 'key': pygame.K_RSHIFT,        'type': self.held,          'function': self.zoom.increase}, 
+            'quote':        {'count': 0, 'key': pygame.K_QUOTE,         'type': self.pressed,       'function': self.zoom.decrease_order}, 
+            'slash':        {'count': 0, 'key': pygame.K_SLASH,         'type': self.pressed,       'function': self.zoom.increase_order}, 
+            'semicolon':    {'count': 0, 'key': pygame.K_SEMICOLON,     'type': self.pressed,       'function': self.zoom.next}, 
+            'period':       {'count': 0, 'key': pygame.K_PERIOD,        'type': self.pressed,       'function': self.zoom.previous}, 
+            'backslash':    {'count': 0, 'key': pygame.K_BACKSLASH,     'type': self.pressed,       'function': self.zoom.auto_scale.increase},
             'w':            {'count': 0, 'key': pygame.K_w,             'type': self.held,          'function': None}, 
             's':            {'count': 0, 'key': pygame.K_s,             'type': self.held,          'function': None}, 
             'a':            {'count': 0, 'key': pygame.K_a,             'type': self.held,          'function': self.craft.rotate_ccw}, 
