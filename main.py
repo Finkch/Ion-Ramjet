9ddd63d193fadb34c355616fff829baca35d2fae
Finkch <skyler@thealdersons.org>
2024-01-02 10:48:35 -0800

    Added a separate simulation loop in main
    This separate loops contains no graphics or printouts

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import keyboard as kb
 
 # Should debug printout
 DEBUG = False
+MAX_SPEED = True
 
 # First, there was nothing.
 # Then, there was "setup".
@@ -23,7 +24,6 @@ def setup():
     #   Initial simulation rate
     #   Framerate
     clock = o.Time(1, 1000 / 60)
-    clock.pause()
 
 
     # Grabs the actors
@@ -43,9 +43,21 @@ def setup():
 # Simulates
 def exist(clock, zoom, screen, keyboard, actors, craft):
 
-    # Keeps track of simulation duration
-    simulate = True
+    if not MAX_SPEED:
+        real_time(clock, zoom, screen, keyboard, actors, craft)
 
+    else:
+        max_speed(clock, zoom, screen, keyboard, actors, craft)
+
+    end_simulation(clock, zoom, screen, keyboard, actors, craft)
+
+
+
+# Simulation aims to match a certain pace.
+# Includes visuals; good for playing around.
+def real_time(clock, zoom, screen, keyboard, actors, craft):
+    
+    simulate = True
 
     # Simulates a workload for a moment to normalise the timer
     clock.real_time.stamp()
@@ -64,7 +76,7 @@ def exist(clock, zoom, screen, keyboard, actors, craft):
 
         # Performs one stpe
         if not clock.paused:
-            step(time_step, actors)
+            gravity_step(time_step, actors)
 
             # Debug printout
             debug(clock, craft)
@@ -82,13 +94,37 @@ def exist(clock, zoom, screen, keyboard, actors, craft):
             # Handles keyboard inputs
             simulate = keyboard()
 
+# Maximises simulation rate.
+def max_speed(clock, zoom, screen, keyboard, actors, craft):
+    
+    
+    simulate = True
 
+    # Sets the timestep
+    time_step = clock.timer.goal
 
+    # Simulates
+    while simulate:
 
+        # Stamps clock to track performance
+        clock()
+
+        # Steps actors forward
+        step(time_step, actors)
+
+        # End condition
+        if clock.steps > 2 * c.day:
+            simulate = False
 
 # One step of simulation
 def step(time_step, actors):
 
+    # Simulates each actor
+    for actor in actors:
+        actor(time_step)
+
+def gravity_step(time_step, actors):
+    
     # Applies gravity
     g.easy_gravity(actors)
 
@@ -96,6 +132,15 @@ def step(time_step, actors):
     for actor in actors:
         actor(time_step)
 
+
+# Things to be done at the end of the simulation
+def end_simulation(clock, zoom, screen, keyboard, actors, craft):
+    
+    clock.real_time.stamp()
+    delta = (clock.real_time.peek() - clock.real_time.start) / 1000
+
+    print(f'Real time: {delta:.2e} s,\tSim time: {clock} -> {clock.sim_time:.2e} s')
+    print(f'Simulation steps per second: {clock.steps / delta:.0f} (recent: {1000 / clock.timer.get_average_difs():.0f})')
 
 # Performs a debug readout
 def debug(clock, craft):
